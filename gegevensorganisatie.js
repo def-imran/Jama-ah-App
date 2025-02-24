97b952a07820312409b8c3dd466afee3089aa7b9
def-imran <imranos1804@gmail.com>
2025-02-24 12:46:44 +0100

    Update gegevensorganisatie.js

--- gegevensorganisatie.js
+++ gegevensorganisatie.js
@@ -1,7 +1,5 @@
-const gaVederknop = document.getElementById("ga-verder-knop");
+const gaVerderKnop = document.getElementById("ga-verder-knop");
 
-gaVederknop.addEventListener("click", function(){
-    if (organisatieKnop.classList.contains("active")){
-        window.location.href = "events.html";
-    }
-})
+gaVerderKnop.addEventListener("click", function () {
+    window.location.href = "events.html";
+});
